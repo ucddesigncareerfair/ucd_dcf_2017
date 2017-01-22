64433f7f043e007879ea9aac2597cae97a900c8a
Derek Dennison <dbdennison@ucdavis.edu>
2017-01-22 00:51:45 -0800

    domain

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,5 +1,10 @@
 module.exports = {
 
+  cname: {
+	files: [
+		{expand: true, cwd: './', src: ['CNAME'], dest: 'dist'},
+	]
+  },
   img: {
 	files: [
 		{expand: true, cwd: 'src/img', src: ['**'], dest: 'dist/img'},
